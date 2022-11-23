d7c5662100e84c647fd0eae646c7d9dc7ee7f0be
Miguel Ferrer <ingferrermiguel@gmail.com>
2022-11-23 11:51:31 -0500

    bump ver

--- setup.py
+++ setup.py
@@ -6,16 +6,19 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-setup(name='bitbucket-python',
-      version='0.2.2',
-      description='API wrapper for Bitbucket written in Python',
-      long_description=read('README.md'),
-      url='https://github.com/GearPlug/bitbucket-python',
-      author='Miguel Ferrer',
-      author_email='ingferrermiguel@gmail.com',
-      license='GPL',
-      packages=['bitbucket'],
-      install_requires=[
-          'requests',
-      ],
-      zip_safe=False)
+setup(
+    name="bitbucket-python",
+    version="0.2.3",
+    description="API wrapper for Bitbucket written in Python",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/GearPlug/bitbucket-python",
+    author="Miguel Ferrer",
+    author_email="ingferrermiguel@gmail.com",
+    license="MIT",
+    packages=["bitbucket"],
+    install_requires=[
+        "requests",
+    ],
+    zip_safe=False,
+)
